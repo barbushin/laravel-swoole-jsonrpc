8fb3c5ba4fc152a54bbe440938d8dc55807125d3
huangyi <huangyi@haochang.tv>
2017-07-17 12:22:05 +0800

    jsonrpc kernel

---
+++ src/Foundation/Kernel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace HuangYi\JsonRpc\Foundation;
+
+use HuangYi\JsonRpc\Contracts\KernelContract;
+
+class Kernel implements KernelContract
+{
+    public function handle(Request $request)
+    {
+        // TODO: Implement handle() method.
+    }
+
+    public function terminal(Request $request, Response $response)
+    {
+        // TODO: Implement terminal() method.
+    }
+}
