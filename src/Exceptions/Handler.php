1ea0859d44c3b598424e13ff0643ea0967f9243f
huangyi <huangyi@haochang.tv>
2017-08-21 16:38:09 +0800

    Exception handler

---
+++ src/Exceptions/Handler.php
@@ -0,0 +1,125 @@
+<?php
+
+/*
+ * This file is part of the huang-yi/laravel-swoole-http package.
+ *
+ * (c) Huang Yi <coodeer@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HuangYi\JsonRpc\Exceptions;
+
+use Exception;
+use HuangYi\JsonRpc\Foundation\Response;
+use Psr\Log\LoggerInterface;
+use Illuminate\Contracts\Container\Container;
+use HuangYi\JsonRpc\Contracts\ExceptionHandlerContract as JsonRpcExceptionHandlerContract;
+use Symfony\Component\Console\Application as ConsoleApplication;
+use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
+
+class Handler implements ExceptionHandlerContract, JsonRpcExceptionHandlerContract
+{
+    /**
+     * The container implementation.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $container;
+
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [];
+
+    /**
+     * Create a new exception handler instance.
+     *
+     * @param \Illuminate\Contracts\Container\Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Report or log an exception.
+     *
+     * @param  \Exception $e
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function report(Exception $e)
+    {
+        if ($this->shouldntReport($e)) {
+            return;
+        }
+
+        try {
+            $logger = $this->container->make(LoggerInterface::class);
+        } catch (Exception $ex) {
+            throw $e;
+        }
+
+        $logger->error($e);
+    }
+
+    /**
+     * Determine if the exception should be reported.
+     *
+     * @param  \Exception $e
+     * @return bool
+     */
+    public function shouldReport(Exception $e)
+    {
+        return ! $this->shouldntReport($e);
+    }
+
+    /**
+     * Determine if the exception is in the "do not report" list.
+     *
+     * @param  \Exception $e
+     * @return bool
+     */
+    protected function shouldntReport(Exception $e)
+    {
+        $dontReport = array_merge($this->dontReport, [ResponseException::class]);
+
+        return ! is_null(collect($dontReport)->first(function ($type) use ($e) {
+            return $e instanceof $type;
+        }));
+    }
+
+    /**
+     * Render an exception into a response.
+     *
+     * @param  \HuangYi\JsonRpc\Foundation\Request $request
+     * @param  \Exception $e
+     * @return \HuangYi\JsonRpc\Foundation\Response
+     */
+    public function render($request, Exception $e)
+    {
+        $code = $e->getCode();
+        $message = $e->getMessage();
+        $data = null;
+
+        if ($e instanceof ResponseException) {
+            $data = $e->getData();
+        }
+
+        return (new Response())->setError($code, $message, $data);
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @param \Exception $e
+     */
+    public function renderForConsole($output, Exception $e)
+    {
+        (new ConsoleApplication)->renderException($e, $output);
+    }
+}
