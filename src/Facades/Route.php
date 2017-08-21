46d4cb53d311e0b25c1750c01f12aed89218a2e7
huangyi <huangyi@haochang.tv>
2017-08-21 17:24:26 +0800

    router facade

--- src/Facades/Route.php
+++ src/Facades/Route.php
@@ -4,7 +4,21 @@ namespace HuangYi\JsonRpc\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \HuangYi\JsonRpc\Routing\Route add(string $method, \Closure | array | string $action)
+ * @method static void group(array $attributes, \Closure $callback)
+ *
+ * @see \HuangYi\JsonRpc\Routing\Router
+ */
 class Route extends Facade
 {
-
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'swoole.jsonrpc.router';
+    }
 }
