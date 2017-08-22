eaf0e1dca7b20026f910ab2087fb66c01508f6c8
huangyi <huangyi@haochang.tv>
2017-08-22 09:53:22 +0800

    changed namespace

---
+++ src/Server/Kernel.php
@@ -0,0 +1,189 @@
+<?php
+
+/*
+ * This file is part of the huang-yi/laravel-swoole-http package.
+ *
+ * (c) Huang Yi <coodeer@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HuangYi\JsonRpc\Server;
+
+use Exception;
+use HuangYi\JsonRpc\Contracts\ExceptionHandlerContract;
+use HuangYi\JsonRpc\Contracts\KernelContract;
+use HuangYi\JsonRpc\Routing\Router;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Contracts\Foundation\Application;
+
+class Kernel implements KernelContract
+{
+    /**
+     * The application implementation.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * The router instance.
+     *
+     * @var \HuangYi\JsonRpc\Routing\Router
+     */
+    protected $router;
+
+    /**
+     * The application's route middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [];
+
+    /**
+     * Create a new JSON-RPC kernel instance.
+     *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     * @param \HuangYi\JsonRpc\Routing\Router $router
+     */
+    public function __construct(Application $app, Router $router)
+    {
+        $this->app = $app;
+        $this->router = $router;
+
+        foreach ($this->routeMiddleware as $key => $middleware) {
+            $router->aliasMiddleware($key, $middleware);
+        }
+    }
+
+    /**
+     * Handle an incoming JSON-RPC request.
+     *
+     * @param  \HuangYi\JsonRpc\Server\Request $request
+     * @return \HuangYi\JsonRpc\Server\Response
+     */
+    public function handle($request)
+    {
+        try {
+            $this->app->instance('swoole.jsonrpc.request', $request);
+
+            Facade::clearResolvedInstance('swoole.jsonrpc.request');
+
+            $response = $this->router->dispatch($request);
+        } catch (Exception $e) {
+            $this->reportException($e);
+
+            $response = $this->renderException($request, $e);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Call the terminate method on any terminable middleware.
+     *
+     * @param  \HuangYi\JsonRpc\Server\Request $request
+     * @param  \HuangYi\JsonRpc\Server\Response $response
+     * @return void
+     */
+    public function terminate($request, $response)
+    {
+        $this->terminateMiddleware($request, $response);
+
+        if (method_exists($this->app, 'terminate')) {
+            $this->app->terminate();
+        }
+    }
+
+    /**
+     * Call the terminate method on any terminable middleware.
+     *
+     * @param \HuangYi\JsonRpc\Server\Request $request
+     * @param \HuangYi\JsonRpc\Server\Response $response
+     * @return void
+     */
+    protected function terminateMiddleware($request, $response)
+    {
+        $middlewares = $this->app->shouldSkipMiddleware() ? [] : $this->gatherRouteMiddleware($request);
+
+        foreach ($middlewares as $middleware) {
+            if (! is_string($middleware)) {
+                continue;
+            }
+
+            list($name, $parameters) = $this->parseMiddleware($middleware);
+
+            $instance = $this->app->make($name);
+
+            if (method_exists($instance, 'terminate')) {
+                $instance->terminate($request, $response);
+            }
+        }
+    }
+
+    /**
+     * Gather the route middleware for the given request.
+     *
+     * @param  \HuangYi\JsonRpc\Server\Request $request
+     * @return array
+     */
+    protected function gatherRouteMiddleware($request)
+    {
+        if ($route = $request->route()) {
+            return $this->router->gatherRouteMiddleware($route);
+        }
+
+        return [];
+    }
+
+    /**
+     * Parse a middleware string to get the name and parameters.
+     *
+     * @param  string $middleware
+     * @return array
+     */
+    protected function parseMiddleware($middleware)
+    {
+        list($name, $parameters) = array_pad(explode(':', $middleware, 2), 2, []);
+
+        if (is_string($parameters)) {
+            $parameters = explode(',', $parameters);
+        }
+
+        return [$name, $parameters];
+    }
+
+    /**
+     * Report the exception to the exception handler.
+     *
+     * @param  \Exception $e
+     * @return void
+     */
+    protected function reportException(Exception $e)
+    {
+        $this->app[ExceptionHandlerContract::class]->report($e);
+    }
+
+    /**
+     * Render the exception to a response.
+     *
+     * @param  \HuangYi\JsonRpc\Server\Request $request
+     * @param  \Exception $e
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function renderException($request, Exception $e)
+    {
+        return $this->app[ExceptionHandlerContract::class]->render($request, $e);
+    }
+
+    /**
+     * Get the Laravel application instance.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application
+     */
+    public function getApplication()
+    {
+        return $this->app;
+    }
+}
---
+++ src/Server/Request.php
@@ -0,0 +1,202 @@
+<?php
+
+/*
+ * This file is part of the huang-yi/laravel-swoole-http package.
+ *
+ * (c) Huang Yi <coodeer@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HuangYi\JsonRpc\Server;
+
+use HuangYi\JsonRpc\Exceptions\InvalidRequestException;
+use HuangYi\JsonRpc\Exceptions\ParseErrorException;
+use HuangYi\JsonRpc\Routing\Route;
+
+class Request
+{
+    /**
+     * JSON-RPC version.
+     *
+     * @var string
+     */
+    protected $jsonrpc = '2.0';
+
+    /**
+     * Request method.
+     *
+     * @var string
+     */
+    protected $method;
+
+    /**
+     * Request parameters.
+     *
+     * @var mixed
+     */
+    protected $params;
+
+    /**
+     * Request id.
+     *
+     * @var mixed
+     */
+    protected $id;
+
+    /**
+     * Required members in JSON-RPC 2.0.
+     *
+     * @var array
+     */
+    protected $requiredMembers = ['jsonrpc', 'method'];
+
+    /**
+     * Optional members in JSON-RPC 2.0.
+     *
+     * @var array
+     */
+    protected $optionalMembers = ['params', 'id'];
+
+    /**
+     * Current route.
+     *
+     * @var \HuangYi\JsonRpc\Routing\Route
+     */
+    protected $route;
+
+    /**
+     * Makes JSON-RPC request.
+     *
+     * @param $payload
+     * @return static
+     * @throws \HuangYi\JsonRpc\Exceptions\ParseErrorException
+     * @throws \HuangYi\JsonRpc\Exceptions\InvalidRequestException
+     */
+    public static function make($payload)
+    {
+        $attributes = static::parseJson($payload);
+
+        return new static($attributes);
+    }
+
+    /**
+     * Request constructor.
+     *
+     * @param array $attributes
+     * @throws \HuangYi\JsonRpc\Exceptions\InvalidRequestException
+     */
+    public function __construct(array $attributes)
+    {
+        $this->initialize($attributes);
+    }
+
+    /**
+     * Sets the parameters for JSON-RPC request.
+     *
+     * @param array $attributes
+     * @throws \HuangYi\JsonRpc\Exceptions\InvalidRequestException
+     */
+    protected function initialize(array $attributes)
+    {
+        foreach ($this->requiredMembers as $member) {
+            if (! array_key_exists($member, $attributes)) {
+                throw new InvalidRequestException;
+            }
+
+            $this->{$member} = $attributes[$member];
+        }
+
+        foreach ($this->optionalMembers as $member) {
+            if (array_key_exists($member, $attributes)) {
+                $this->{$member} = $attributes[$member];
+            }
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getJsonrpc()
+    {
+        return $this->jsonrpc;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    /**
+     * Get parameter value.
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public function getParam($key, $default = null)
+    {
+        return array_get($this->params, $key, $default);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the route handling the request.
+     *
+     * @return \HuangYi\JsonRpc\Routing\Route
+     */
+    public function getRoute()
+    {
+        return $this->route;
+    }
+
+    /**
+     * Set the route resolver callback.
+     *
+     * @param  \HuangYi\JsonRpc\Routing\Route $route
+     * @return $this
+     */
+    public function setRoute(Route $route)
+    {
+        $this->route = $route;
+
+        return $this;
+    }
+
+    /**
+     * Parse json.
+     *
+     * @param string $payload
+     * @return array
+     * @throws \HuangYi\JsonRpc\Exceptions\ParseErrorException
+     */
+    public static function parseJson($payload)
+    {
+        $array = json_decode($payload, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new ParseErrorException;
+        }
+
+        return $array;
+    }
+}
